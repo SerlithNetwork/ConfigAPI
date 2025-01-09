916985db0d39db48ec41b1d2bf9efcdc7e60be0f
J4C0B3Y <J4C0B3Y@gmail.com>
2025-01-10 09:07:01 +1100

    fix spigotapi dependency

--- bukkit/build.gradle.kts
+++ bukkit/build.gradle.kts
@@ -1,8 +1,9 @@
 
 repositories {
-    maven("https://papermc.io/repo/repository/maven-public/")
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven("https://oss.sonatype.org/content/repositories/snapshots/")
 }
 
 dependencies {
-    compileOnly("com.destroystokyo.paper:paper-api:1.12.2-R0.1-SNAPSHOT")
+    compileOnly("org.spigotmc:spigot-api:1.8.8-R0.1-SNAPSHOT")
 }
