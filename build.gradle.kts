bae47cff1077ab0c437bbaa116584ac8bf8ad00e
J4C0B3Y <J4C0B3Y@gmail.com>
2025-08-17 15:50:13 +1000

    bump version to 1.2.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 object Project {
     const val NAME = "ConfigAPI"
     const val GROUP = "net.j4c0b3y"
-    const val VERSION = "1.2.4"
+    const val VERSION = "1.2.5"
 }
 
 allprojects {
